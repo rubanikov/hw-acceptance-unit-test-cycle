c525ba37a5a2a458939575f450a400c00cac0ceb
Ubuntu <ubuntu@ip-172-31-88-109.ec2.internal>
2020-10-11 22:05:49 +0000

    part02_complete

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,8 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  
+    # New function for HW4
+  get 'movies/director/:id', to: 'movies#director', as: 'movies_director'
+  
 end
